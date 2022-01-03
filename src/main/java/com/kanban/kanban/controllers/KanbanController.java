e57f34d7539de830af0fd4c96ddaa9643dc2342f
marcos <marcosviniciocardoso123@gmail.com>
2022-01-03 12:52:37 -0300

    this commit add read KanbanBoard method in controller

--- src/main/java/com/kanban/kanban/controllers/KanbanController.java
+++ src/main/java/com/kanban/kanban/controllers/KanbanController.java
@@ -1,5 +1,7 @@
 package com.kanban.kanban.controllers;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
@@ -7,10 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kanban.kanban.exceptions.CreateKanbanBoardException;
+import com.kanban.kanban.exceptions.ReadKanbanBoardException;
 import com.kanban.kanban.models.KanbanBoard;
 import com.kanban.kanban.models.Response;
 import com.kanban.kanban.services.DAO;
@@ -24,7 +29,7 @@ public class KanbanController {
 		this.service = service;
 	}
 	
-	@GetMapping
+	@PostMapping
 	public ResponseEntity<Response<KanbanBoard>> create(@Valid @RequestBody KanbanBoard kanbanBoard, BindingResult result){
 		try {
 			
@@ -40,11 +45,32 @@ public class KanbanController {
 			}
 			
 		}catch (CreateKanbanBoardException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
 			return new ResponseEntity<Response<KanbanBoard>>(HttpStatus.CONFLICT);
 		}catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
 			return new ResponseEntity<Response<KanbanBoard>>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 	
+	@GetMapping
+	public ResponseEntity<Response<List<KanbanBoard>>> read(@RequestParam(required = false) int skip, @RequestParam(required = false) int limit, @RequestParam String kanbanBoardId){
+		try {
+			
+			Response<List<KanbanBoard>> response = service.readKanbanBoard(skip, limit, kanbanBoardId);
+			return new ResponseEntity<Response<List<KanbanBoard>>>(response, HttpStatus.OK);
+			
+		}catch (ReadKanbanBoardException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			return new ResponseEntity<Response<List<KanbanBoard>>>(HttpStatus.CONFLICT);
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			return new ResponseEntity<Response<List<KanbanBoard>>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 	
 }
