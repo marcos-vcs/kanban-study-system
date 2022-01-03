b20b19432a77e876219e2d904996e8d63793c6b2
marcos <marcosviniciocardoso123@gmail.com>
2022-01-03 12:43:21 -0300

    this commit add create method in controller

--- src/main/java/com/kanban/kanban/controllers/KanbanController.java
+++ src/main/java/com/kanban/kanban/controllers/KanbanController.java
@@ -1,5 +1,50 @@
 package com.kanban.kanban.controllers;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kanban.kanban.exceptions.CreateKanbanBoardException;
+import com.kanban.kanban.models.KanbanBoard;
+import com.kanban.kanban.models.Response;
+import com.kanban.kanban.services.DAO;
+
+@Controller
+@RequestMapping("/kanban-api")
 public class KanbanController {
 
+	private final DAO service;
+	public KanbanController(DAO service) {
+		this.service = service;
+	}
+	
+	@GetMapping
+	public ResponseEntity<Response<KanbanBoard>> create(@Valid @RequestBody KanbanBoard kanbanBoard, BindingResult result){
+		try {
+			
+			if(result.hasErrors()) {
+				return new ResponseEntity<Response<KanbanBoard>>(HttpStatus.NOT_ACCEPTABLE);
+			}
+			
+			Response<KanbanBoard> response = service.createKanbanBoard(kanbanBoard);
+			if(response == null) {
+				return new ResponseEntity<Response<KanbanBoard>>(HttpStatus.NOT_MODIFIED);
+			}else {
+				return new ResponseEntity<Response<KanbanBoard>>(response, HttpStatus.OK);
+			}
+			
+		}catch (CreateKanbanBoardException e) {
+			return new ResponseEntity<Response<KanbanBoard>>(HttpStatus.CONFLICT);
+		}catch (Exception e) {
+			return new ResponseEntity<Response<KanbanBoard>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	
 }
--- src/main/java/com/kanban/kanban/models/KanbanBoard.java
+++ src/main/java/com/kanban/kanban/models/KanbanBoard.java
@@ -2,6 +2,10 @@ package com.kanban.kanban.models;
 
 import java.util.List;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -21,6 +25,9 @@ public class KanbanBoard {
 	
 	@Id
 	private String id;
+	@NotNull
+	@Max(value = 300)
+	@Min(value = 5)
 	private String title;
 	private List<KanbanDemand> demands;
 	
