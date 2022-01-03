8ea69b1894b4278489bfc1486ffbc7f741c42002
marcos <marcosviniciocardoso123@gmail.com>
2022-01-03 14:02:45 -0300

    this commit add delete method in controller

--- src/main/java/com/kanban/kanban/controllers/KanbanController.java
+++ src/main/java/com/kanban/kanban/controllers/KanbanController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.kanban.kanban.exceptions.CreateKanbanBoardException;
+import com.kanban.kanban.exceptions.DeleteKanbanBoardException;
 import com.kanban.kanban.exceptions.ReadKanbanBoardException;
 import com.kanban.kanban.exceptions.UpdateKanbanBoardException;
 import com.kanban.kanban.models.KanbanBoard;
@@ -91,5 +93,22 @@ public class KanbanController {
 		}
 	}
 	
+	@DeleteMapping
+	public ResponseEntity<Response<KanbanBoard>> delete(@RequestParam String kanbanBoardId){
+		try {
+			
+			Response<KanbanBoard> response = service.deleteKanbanBoard(kanbanBoardId);
+			return new ResponseEntity<Response<KanbanBoard>>(response, HttpStatus.OK);
+			
+		}catch (DeleteKanbanBoardException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			return new ResponseEntity<Response<KanbanBoard>>(HttpStatus.CONFLICT);
+		}catch (RuntimeException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			return new ResponseEntity<Response<KanbanBoard>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 	
 }
